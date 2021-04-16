5c28c3d4bb4360202784c4ba44ae0c4d0d3c2582
Art-byte <arturo_pdrzbn@hotmail.com>
2021-04-15 22:47:23 -0500

    Finalizado

---
+++ src/test/java/com/example/ArtCom/sqlConnections/SqlConnectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ArtCom.sqlConnections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlConnectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
